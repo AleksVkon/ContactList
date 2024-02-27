60f9c6dd5a0f5e03e1587d4e71f6e5402ed7ae68
Aleksey Konchyts <konchits.aleksey@gmail.com>
2024-02-27 15:05:59 +0300

    Initial Commit

---
+++ ContactList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ContactList
+//
+//  Created by Aleksey Konchyts on 27.02.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
