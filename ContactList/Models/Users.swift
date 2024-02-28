99d1d8f46dfa11bfd6f7bf732ab1735e8a37c771
Aleksey Konchyts <konchits.aleksey@gmail.com>
2024-02-28 12:16:11 +0300

    with getUser

--- ContactList/Models/Users.swift
+++ ContactList/Models/Users.swift
@@ -13,20 +13,32 @@ struct User {
     var phone: String
     var email: String
     
-    static func getUser() -> User? {
-        guard let userName = names.randomElement(),
-              let userSurname = surnames.randomElement(),
-              let userPhone = phoneNumbers.randomElement(),
-              let userEmail = emails.randomElement()
-        else {
-            return nil
-        }
+    static func getUsers() -> [User]? {
+        var users: [User] = []
         
-        return User(
-            name: userName,
-            surname: userSurname,
-            phone: userPhone,
-            email: userEmail
-        )
+        for _ in 1...names.count {
+            guard let userName = names.randomElement(),
+                  let userSurname = surnames.randomElement(),
+                  let userPhone = phoneNumbers.randomElement(),
+                  let userEmail = emails.randomElement()
+            else {
+                return nil
+            }
+
+            let user = User(
+                name: userName,
+                surname: userSurname,
+                phone: userPhone,
+                email: userEmail
+            )
+            
+            users.append(user)
+            
+            names.removeAll { $0 == userName }
+            surnames.removeAll { $0 == userSurname }
+            phoneNumbers.removeAll { $0 == userPhone }
+            emails.removeAll { $0 == userEmail }
+        }
+        return users
     }
 }
